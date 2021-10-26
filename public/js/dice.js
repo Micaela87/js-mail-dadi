322cb0d37922b5568901fd2c47d157e9a925d5e6
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-10-26 17:38:54 +0200

    dice game interface completed

--- public/js/dice.js
+++ public/js/dice.js
@@ -1,15 +1,36 @@
 // generate and confront two random numbers; the highest wins
+const buttonToPlay = document.querySelector('.generate-number');
+const userResult = document.querySelector('.user-number');
+const computerResult = document.querySelector('.roby-number');
+const finalResult = document.querySelector('.result');
+const buttonToReset = document.querySelector('.reset-game');
 
-const userNumber = Math.floor(Math.random() * 6) + 1;
+const generateRandomNumber = () => {
+    // generates and displays user random number
+    const userNumber = Math.floor(Math.random() * 6) + 1;
+    userResult.innerHTML += ` ${userNumber}`;
+    // generates and displays computer random number
+    const computerNumber = Math.floor(Math.random() * 6) + 1;
+    computerResult.innerHTML += ` ${computerNumber}`;
 
-const computerNumber = Math.floor(Math.random() * 6) + 1;
+    finalResult.style.display = 'block';
 
-console.log(userNumber, computerNumber);
-
-if (userNumber > computerNumber) {
-    console.log('You won')
-} else if (userNumber < computerNumber) {
-    console.log('Computer won')
-} else {
-    console.log('Same number. Nobody won')
+    // checks user number against computer number
+    if (userNumber > computerNumber) {
+        finalResult.innerHTML = 'You won!';
+    } else if (userNumber < computerNumber) {
+        finalResult.innerHTML = 'Roby won!';
+    } else {
+        finalResult.innerHTML = 'Same number! You both won!';
+    }
 }
+
+const resetGame = () => {
+    userResult.innerHTML = 'Your number is';
+    computerResult.innerHTML = 'Roby\'s number is';
+    finalResult.style.display = 'none';
+    finalResult.innerHTML = '';
+}
+
+buttonToPlay.onclick = generateRandomNumber;
+buttonToReset.onclick = resetGame;
