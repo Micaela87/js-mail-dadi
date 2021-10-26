974ff3185da5fa3c667b8da08057e26d2b3eac9c
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-10-26 19:55:07 +0200

    dice game done even better

--- public/js/dice.js
+++ public/js/dice.js
@@ -1,7 +1,9 @@
 // generate and confront two random numbers; the highest wins
 
 // selects HTML elements
+const containerToPlay = document.querySelector('.first-container'); // container which stores the rules of the game
 const buttonToPlay = document.querySelector('.generate-number'); // button to be clicked to play
+const containerForResults = document.querySelector('.second-container'); // container which stores the results
 const userResult = document.querySelector('.user-number'); // div to display user number
 const computerResult = document.querySelector('.roby-number'); // div to display computer number
 const finalResult = document.querySelector('.result'); // div to display the winner
@@ -17,9 +19,6 @@ const generateRandomNumber = () => {
     const computerNumber = Math.floor(Math.random() * 6) + 1;
     computerResult.innerHTML += ` ${computerNumber}`;
 
-    // displays winner
-    finalResult.style.display = 'block';
-
     // checks user number against computer number
     if (userNumber > computerNumber) {
         finalResult.innerHTML = 'You won!';
@@ -29,19 +28,20 @@ const generateRandomNumber = () => {
         finalResult.innerHTML = 'Same number! You both won!';
     }
 
-    // hides buttonToPlay
-    buttonToPlay.classList.add('inactive');
-    buttonToReset.classList.remove('inactive');
-    
+    // hides first container
+    containerToPlay.classList.add('inactive');
+
+    // displays results and the button to reset the game
+    containerForResults.classList.remove('inactive');
+
 }
 
 // resets all the paramaters
 const resetGame = () => {
     userResult.innerHTML = 'Your number is';
     computerResult.innerHTML = 'Roby\'s number is';
-    finalResult.style.display = 'none';
-    buttonToPlay.classList.remove('inactive');
-    buttonToReset.classList.add('inactive');
+    containerForResults.classList.add('inactive');
+    containerToPlay.classList.remove('inactive');
 }
 
 // adds click event on buttons
